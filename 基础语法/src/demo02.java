538d0879ff7a5929f4e23a98a883808d5891d111
sunbowen <857153966@qq.com>
2021-06-08 21:54:58 +0800

    123

--- 基础语法/src/demo02.java
+++ 基础语法/src/demo02.java
@@ -1,2 +1,13 @@
-public class demo01 {
+public class demo02 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        byte num2 = 10;
+        short num3 = 10;
+        long num4 = 10L;
+
+        float num5 = 2.4F;
+        double num6 = 2.3434;
+
+        char name = 'a';
+    }
 }
