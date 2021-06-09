b14d04308beac6b6c47a67ce855d2f95dff9b366
sunbowen <857153966@qq.com>
2021-06-09 14:24:07 +0800

    doc文档

--- 基础语法/src/base/Doc.java
+++ 基础语法/src/base/Doc.java
@@ -1,4 +1,19 @@
 package base;
-
+/**
+ * @author sunbowen
+ * @version 1.0
+ * @since 1.8
+ * */
 public class Doc {
+
+    String name;
+
+    /**
+     *
+     * @param name
+     * @return
+     */
+    public String test(String name) {
+        return name;
+    }
 }
